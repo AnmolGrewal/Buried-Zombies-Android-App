eb637f10e2e830509420639eed813e53f6322abc
Anmo <anmolsgrewal@gmail.com>
2018-02-18 00:07:35 -0800

    Added Background animation now working on buttons

--- app/src/main/java/as3/cmpt276/findtheburiedzombies/welcomeScreen.java
+++ app/src/main/java/as3/cmpt276/findtheburiedzombies/welcomeScreen.java
@@ -1,7 +1,13 @@
 package as3.cmpt276.findtheburiedzombies;
 
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+//https://stackoverflow.com/questions/6533942/adding-gif-image-in-an-imageview-in-android
+//https://stackoverflow.com/questions/29047902/how-to-add-an-image-to-the-drawable-folder-in-android-studio
+//https://stackoverflow.com/questions/15874117/how-to-set-delay-in-android
 
 public class welcomeScreen extends AppCompatActivity {
 
@@ -9,6 +15,15 @@ public class welcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
-        //Branch Test
+
+        //Animation lasts about 2seconds so 4seconds after
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Do something after 6s = 6000ms
+
+            }
+        }, 6000);
     }
 }
