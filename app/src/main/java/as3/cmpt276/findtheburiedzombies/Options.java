f35b0462ee15bc52d5130adee4ac783d89473189
Anmo <anmolsgrewal@gmail.com>
2018-02-18 05:07:43 -0800

    Options half done

--- app/src/main/java/as3/cmpt276/findtheburiedzombies/Options.java
+++ app/src/main/java/as3/cmpt276/findtheburiedzombies/Options.java
@@ -1,7 +1,13 @@
 package as3.cmpt276.findtheburiedzombies;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Options extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class Options extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
+
+        createRadioButtons();
+    }
+
+    private void createRadioButtons() {
+        RadioGroup group = findViewById(R.id.numMines);
+        int[] numMines = getResources().getIntArray(R.array.num_mines);
+
+        for(int i = 0; i < numMines.length; i++) {
+            final int numberMine = numMines[i];
+
+            RadioButton button = new RadioButton(this);
+            button.setText(numberMine + " Mines");
+            button.setTextColor(Color.WHITE);
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(Options.this, "You Clicked " + numberMine, Toast.LENGTH_SHORT)
+                        .show();
+                }
+            });
+            group.addView(button);
+        }
+
+
+
     }
 }
