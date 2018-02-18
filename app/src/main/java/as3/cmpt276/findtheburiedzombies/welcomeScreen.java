e33da7a307ad8b3170b5ac131f331b12e4dcaeeb
Anmo <anmolsgrewal@gmail.com>
2018-02-17 19:36:02 -0800

    Testing Branch 1

--- app/src/main/java/as3/cmpt276/findtheburiedzombies/welcomeScreen.java
+++ app/src/main/java/as3/cmpt276/findtheburiedzombies/welcomeScreen.java
@@ -9,5 +9,6 @@ public class welcomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        //Branch Test
     }
 }
