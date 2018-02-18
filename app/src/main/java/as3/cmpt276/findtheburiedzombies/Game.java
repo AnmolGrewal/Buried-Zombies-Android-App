3858acf87bcd4bedc645012776a51027da5c8896
Anmo <anmolsgrewal@gmail.com>
2018-02-18 02:12:22 -0800

    Main Menu Activity Done

---
+++ app/src/main/java/as3/cmpt276/findtheburiedzombies/Game.java
@@ -0,0 +1,13 @@
+package as3.cmpt276.findtheburiedzombies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Game extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
